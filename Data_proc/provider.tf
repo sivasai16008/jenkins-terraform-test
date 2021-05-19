d02d9d3c0dc0f83b08212e2c7d7c9d00192ba018
sivasai16008 <48430092+sivasai16008@users.noreply.github.com>
2021-05-19 14:33:35 +0530

    Update provider.tf

--- Data_proc/provider.tf
+++ Data_proc/provider.tf
@@ -1,6 +1,6 @@
 # Specify the provider (GCP, AWS, Azure)
 provider "google" {
 credentials = file("credentials.json")
-project = "vivid-carrier-308607"
+project = "arctic-system-312209"
 region = "us-central1"
 }
